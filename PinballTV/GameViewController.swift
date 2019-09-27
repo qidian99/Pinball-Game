26f199cba26e9806303145fb72240dfa5ecbeb67
Dian Qi <qidian990107@163.com>
2019-09-26 22:44:58 -0700

    Initial Commit

---
+++ PinballTV/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  PinballTV
+//
+//  Created by Dian "Charles" Qi on 26/09/2019.
+//  Copyright © 2019 Dian "Charles" Qi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
